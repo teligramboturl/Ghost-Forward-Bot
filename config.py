2df0ebc1692a131fda69217bb6a229946c9f0f73
teligramboturl <teligramboturl@gmail.com>
2025-12-28 15:17:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,16 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "23673651"))
+    API_HASH = environ.get("API_HASH", "f032bfa12ee46e1283f6fb23cfca5c6b")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "ghostbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://dubeyji0995:Gqaqduzpm98kT6ZF@cluster0.ekxizvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "ghost-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    BOT_OWNER = int(environ.get("BOT_OWNER", "Save restricted channel:
+-1003047586469
+
+6677821706"))
 
 # Don't Remove Credit Tg - @Tech_Shreyansh1
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@TechShreyansh
